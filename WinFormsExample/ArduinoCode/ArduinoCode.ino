5254009f1d6d53e0f71df441f2db5343b4aa1ce8
Uwe <Uwe.GitRepos@topmail-files.de>
2019-07-27 14:00:15 +0200

    Arduino code updated; added LED 13 control

--- WinFormsExample/ArduinoCode/ArduinoCode.ino
+++ WinFormsExample/ArduinoCode/ArduinoCode.ino
@@ -1,30 +1,47 @@
 String msg;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  while(Serial.available()) {
-    if (Serial.available() > 0) {
+void loop()
+{
+  while(Serial.available())
+  {
+    if (Serial.available() > 0)
+    {
       char c = Serial.read();
-      if (c == '\n') {
+      if (c == '\n')
+      {
         readMessage();
-      } else {
+      }
+      else
+      {
         msg += c;    
       }
     }  
   }
+  Serial.print("LED13State");
+  Serial.println(digitalRead(LED_BUILTIN));
+  delay(100);
 }
 
-void readMessage() {
-   if (msg == "command1") {
-      // do something here
-      Serial.println("result1");
-    } else if (msg == "command2") {
-      // do something here
-      Serial.println("result2");
-    } else {
+void readMessage()
+{
+   if ((msg == "LED13ON") || (msg == "on"))
+   {
+      digitalWrite(LED_BUILTIN, true);
+      Serial.println("LED13 switched on");
+    }
+    else if ((msg == "LED13OFF") || (msg == "off"))
+    {
+      digitalWrite(LED_BUILTIN, false);
+      Serial.println("LED13 switched on");
+    }
+    else
+    {
       Serial.println("Unknown command: " + msg);  
     }
 
